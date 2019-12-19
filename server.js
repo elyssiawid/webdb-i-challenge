e145f4ab72a807f8763cc113fdbd31d1dcf1c7f3
Elyssia Widjaja <Elyssia@elyssias-mbp.lan>
2019-12-18 17:54:44 -0800

    added get api and get api by id endpoints

--- server.js
+++ server.js
@@ -6,4 +6,20 @@ const server = express();
 
 server.use(express.json());
 
+server.get('/api', (req, res) => {
+    db('accounts')
+    .then(accounts => res.status(200).json(accounts))
+    .catch(err => res.status(500).json({message:'Failed to get accounts'}))
+});
+
+server.get('/api/:id', (req,res) => {
+db('accounts')
+.where({ id: req.params.id})
+.first()
+.then(accts => res.status.(200).json(accts))
+.catch(err => res.status(500).json({message:'Failed to get account'}))
+});
+
+
+
 module.exports = server;
