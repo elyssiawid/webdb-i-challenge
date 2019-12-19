03e7e9e18924242cc776a381d7fbd976ce27c062
Elyssia Widjaja <Elyssia@elyssias-mbp.lan>
2019-12-18 18:26:35 -0800

    mvp finished

--- server.js
+++ server.js
@@ -1,25 +1,63 @@
-const express = require('express');
+const express = require("express");
 
-const db = require('./data/dbConfig.js');
+const db = require("./data/dbConfig.js");
 
 const server = express();
 
 server.use(express.json());
 
-server.get('/api', (req, res) => {
-    db('accounts')
+server.get("/api", (req, res) => {
+  db("accounts")
     .then(accounts => res.status(200).json(accounts))
-    .catch(err => res.status(500).json({message:'Failed to get accounts'}))
+    .catch(err => res.status(500).json({ message: "Failed to get accounts" }));
 });
 
-server.get('/api/:id', (req,res) => {
-db('accounts')
-.where({ id: req.params.id})
-.first()
-.then(accts => res.status.(200).json(accts))
-.catch(err => res.status(500).json({message:'Failed to get account'}))
+server.get("/api/:id", (req, res) => {
+  db("accounts")
+    .where({ id: req.params.id })
+    .first()
+    .then(accts => res.status(200).json(accts))
+    .catch(err => res.status(500).json({ message: "Failed to get account" }));
 });
 
+server.post("/api", (req, res) => {
+  const acctData = req.body;
+  !acctData.name || !acctData.budget
+    ? res.status(400).json({
+        errorMessage: "Please provide name and budget for the account."
+      })
+    : db("accounts")
+        .insert(acctData, "id")
+        .then(([id]) => {
+          db("accounts")
+            .where({ id })
+            .first()
+            .then(acct => {
+              res.status(200).json(acct);
+            });
+        })
+        .catch(err =>
+          res.status(500).json({ message: "Error adding account" })
+        );
+});
 
+server.put("/api/:id", (req, res) => {
+  const acctChange = req.body;
+  db("accounts")
+    .where("id", req.params.id)
+    .update(acctChanges)
+    .then(response => res.status(200).json({ message: "Account updated" }))
+    .catch(err => res.status(500).json({ message: "Error adding account" }));
+});
+
+server.delete("/api/:id", (req, res) => {
+  db("accounts")
+    .where({ id: req.params.id })
+    .del()
+    .then(count =>
+      res.status(200).json({ message: "Deleted records: ${count}" })
+    )
+    .catch(err => res.status(500).json({ message: "Failed to get account" }));
+});
 
 module.exports = server;
